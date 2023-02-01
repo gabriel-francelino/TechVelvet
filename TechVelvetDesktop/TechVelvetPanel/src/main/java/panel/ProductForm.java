fe4622567a3271452b50197e16ef7814a72234c3
Gabriel Piva <89921667+gpiiva@users.noreply.github.com>
2023-02-01 20:36:52 -0300

    Form with JavaSwing to create a new product

---
+++ TechVelvetDesktop/TechVelvetPanel/src/main/java/panel/ProductForm.java
@@ -0,0 +1,89 @@
+package panel;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Serial;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ProductForm extends JFrame {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ProductForm() {
+        setSize(500, 210);
+        setTitle("Create a new product for TechVelvet");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setResizable(false);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 2, 0, 3));
+
+        JLabel headerLabel = new JLabel("Create a new product for TechVelvet");
+        headerLabel.setForeground(Color.decode("#6F4A8E"));
+        headerLabel.setHorizontalAlignment(JLabel.CENTER);
+        add(headerLabel);
+
+        JLabel nameLabel = new JLabel("Name");
+        nameLabel.setForeground(Color.decode("#6F4A8E"));
+        nameLabel.setHorizontalAlignment(JLabel.LEFT);
+        panel.add(nameLabel);
+
+        JTextField nameInput = new JTextField(20);
+        nameInput.setBackground(Color.decode("#EBEBEB"));
+        panel.add(nameInput);
+
+        JLabel priceLabel = new JLabel("Price");
+        priceLabel.setForeground(Color.decode("#6F4A8E"));
+        priceLabel.setHorizontalAlignment(JLabel.LEFT);
+        panel.add(priceLabel);
+
+        JTextField priceInput = new JTextField(20);
+        priceInput.setBackground(Color.decode("#EBEBEB"));
+        panel.add(priceInput);
+
+        JLabel descriptionLabel = new JLabel("Description");
+        descriptionLabel.setForeground(Color.decode("#6F4A8E"));
+        descriptionLabel.setHorizontalAlignment(JLabel.LEFT);
+        panel.add(descriptionLabel);
+
+        JTextField descriptionInput = new JTextField(20);
+        descriptionInput.setBackground(Color.decode("#EBEBEB"));
+        panel.add(descriptionInput);
+
+        JLabel brandLabel = new JLabel("Brand");
+        brandLabel.setForeground(Color.decode("#6F4A8E"));
+        brandLabel.setHorizontalAlignment(JLabel.LEFT);
+        panel.add(brandLabel);
+
+        JTextField brandInput = new JTextField(20);
+        brandInput.setBackground(Color.decode("#EBEBEB"));
+        panel.add(brandInput);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBackground(Color.decode("#6F4A8E"));
+        submitButton.setForeground(Color.WHITE);
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = nameInput.getText();
+                String price = priceInput.getText();
+                String description = descriptionInput.getText();
+                String brand = brandInput.getText();
+                System.out.println("Name: " + name + "\nPrice: " + price + "\nDescription: " + description + "\nBrand: " + brand);
+            }
+        });
+
+        add(panel);
+        add(submitButton);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new ProductForm();
+    }
+}
