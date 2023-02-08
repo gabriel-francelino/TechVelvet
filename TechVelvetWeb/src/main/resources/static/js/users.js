17edd367282b7df15bed20b516f46c6165578c17
Gabriel Piva <89921667+gpiiva@users.noreply.github.com>
2023-02-07 21:29:34 -0300

    Connecting to the API with JS and listing users

--- TechVelvetWeb/src/main/resources/static/js/users.js
+++ TechVelvetWeb/src/main/resources/static/js/users.js
@@ -6,14 +6,39 @@
 
 // HTML Elements
 
+const userList = document.querySelector(".users .list");
 
 // -------------------------------------------------------------------------- 
 
-//  
+// API
+
+const getUsers = async () => {
+    const response = await fetch("/api/users");
+    const users = await response.json();
+    return users; 
+}
+
+const loadUsers = async (ini, max) => {
+    const users = await getUsers();
+    for(let i = ini; i < max; i++) {
+        userList.innerHTML += `
+        <div class="user">
+            <div class="display">
+                <span>${users[i].firstName} ${users[i].lastName}</span>
+            </div>
+            <div class="buttons">
+                <button><i class='bx bxs-pencil'></i></button>
+                <button><i class='bx bxs-trash'></i></button>
+            </div>
+        </div>
+        `;
+    }
+}
 
 // --------------------------------------------------------------------------
 
 // Events
 
+loadUsers(0, 5);
 
 // --------------------------------------------------------------------------
